11cd02e9fa636b23b85c9392f6ff585a5bc1efe5
Jordan <jordan.gonzales17@hotmail.com>
2024-09-26 19:09:47 -0700

    Public JS folder fully set

--- models/index.js
+++ models/index.js
@@ -1,17 +1,31 @@
-const Sequelize = require("sequelize");
-const sequelize = require("../config/sequelize");
 const User = require("./User");
 const Post = require("./Post");
+const Comment = require("./comment");
 
 User.hasMany(Post, {
-  foreignKey: "userId", 
-  onDelete: "CASCADE", 
+  foreignKey: "user_id",
+  onDelete: "CASCADE",
 });
+
 Post.belongsTo(User, {
-  foreignKey: "userId", 
+  foreignKey: "user_id",
 });
-module.exports = {
-  User,
-  Post,
-  sequelize,
-};
+
+Post.hasMany(Comment, {
+  foreignKey: "post_id",
+  onDelete: "CASCADE",
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: "post_id",
+});
+
+Comment.belongsTo(User, {
+  foreignKey: "user_id",
+});
+
+User.hasMany(Comment, {
+  foreignKey: "user_id",
+});
+
+module.exports = { User, Post, Comment };
